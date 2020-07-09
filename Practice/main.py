a45e1111bb459b336d46ec2ce190e35f0bf7f338
Ace Alrich <hex0@live.com>
2020-07-09 07:20:11 +0100

    Define weights and output

--- Practice/main.py
+++ Practice/main.py
@@ -1,5 +1,30 @@
 import numpy as np
 
-a = np.arange(15).reshape(3, 5)
 
-print(a)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+training_inputs = np.array([[0, 0, 1],
+                            [1, 1, 1],
+                            [1, 0, 1],
+                            [0, 1, 1]])
+
+training_outputs = np.array([[0, 1, 1, 0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+print("Synaptic: ")
+print(synaptic_weights)
+
+for iteration in range(1):
+
+    input_layer = training_inputs
+
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+print("Outputs: ")
+print(outputs)
+
