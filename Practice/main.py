175b22defbdd1edc877b7948ad678c8f3fb330d1
Ace Alrich <hex0@live.com>
2020-07-09 07:26:32 +0100

    adjustments for errors

--- Practice/main.py
+++ Practice/main.py
@@ -4,6 +4,8 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+    return  x * (1 - x)
 
 training_inputs = np.array([[0, 0, 1],
                             [1, 1, 1],
@@ -19,11 +21,20 @@ synaptic_weights = 2 * np.random.random((3, 1)) - 1
 print("Synaptic: ")
 print(synaptic_weights)
 
-for iteration in range(1):
+for iteration in range(600000):
 
     input_layer = training_inputs
 
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+    error = training_outputs - outputs
+
+    adjustments = error * sigmoid_derivative(outputs)
+
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+print("After: ")
+print(synaptic_weights)
 
 print("Outputs: ")
 print(outputs)
